2c717f7c1ec5b5ee285f58b8f8f6d1a4159aa772
Padmakumar18 <padmakumar41759@gmail.com>
2025-04-03 22:35:47 +0530

    38

---
+++ SumOfDiagonals.java
@@ -0,0 +1,46 @@
+//package SecondRound;
+
+// 3
+// 7 8 9
+// 6 1 2
+// 5 4 3
+
+// Output : 25
+
+//spiral matrix
+
+public class SumOfDiagonals
+{
+    public static void main(String[] args)
+    {
+        int n = 9 ;
+        int num = n * n ;
+        int row = 0;
+        int col = n-1;
+        int[][] arr = new int[n][n];
+    }
+
+    public static void Right_To_Left(int[][] arr,int row,int col,int num)
+    {
+        if(num==1)
+            return;
+    }
+
+    public static void Up_To_Down(int[][] arr,int row,int col,int num)
+    {
+        if(num==1)
+            return;
+    }
+
+    public static void Left_To_Right(int[][] arr,int row,int col,int num)
+    {
+        if(num==1)
+            return;
+    }
+
+    public static void Down_To_Up(int[][] arr,int row,int col,int num)
+    {
+        if(num==1)
+            return;
+    }
+}
